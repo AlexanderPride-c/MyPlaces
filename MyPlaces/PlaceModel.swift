dfb874c8418fad4e371192b1e4ef3ca17ce509b1
a.pride <a.pride.msk@gmail.com>
2020-08-02 18:58:29 +0300

    Work with model

---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,36 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by Александр Прайд on 02.08.2020.
+//  Copyright © 2020 Alexander Pride. All rights reserved.
+//
+
+import UIKit
+
+struct Place {
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+    
+    static let restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "Love&Life", "Шок", "Бочка"
+    ]
+    
+    static func getPlaces() -> [Place] {
+        
+        var places = [Place]()
+        
+        for place in restaurantNames {
+            places.append(Place(name: place, location: "Уфа", type: "Ресторан", image: place))
+        }
+        
+        return places
+    }
+    
+  
+}
