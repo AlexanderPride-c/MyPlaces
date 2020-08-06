ac53370bb8d859a4aab970a23313a0c5dcc5683b
a.pride <a.pride.msk@gmail.com>
2020-08-06 08:44:10 +0300

    Map View Controller

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Александр Прайд on 06.08.2020.
+//  Copyright © 2020 Alexander Pride. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+    
+}
